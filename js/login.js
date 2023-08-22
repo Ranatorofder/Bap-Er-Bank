4f7dac959e24d0c35dd0bc55b3f329018b5ce197
Ranatorofder <Bayezidalrabbirana@gmail.com>
2023-08-22 14:51:11 +0600

    bank inside

--- js/login.js
+++ js/login.js
@@ -5,10 +5,10 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     const passwordFild = document.getElementById('user-password');
     const password = passwordFild.value;
     if (email === 'sontan@baap.com' && password === 'secret') {
-        console.log('valid user')
+        window.location.href = 'bank.html'
     }
     else {
-        console.log('invalid user')
+        alert('Tui password vule gesos !!. Toke ami teijjo sontan gosona korlam. ')
     }
 
 })
